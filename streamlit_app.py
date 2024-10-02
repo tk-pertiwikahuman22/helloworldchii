f629914652814f5936bf7cc234bb26b1ef1c4f2e
tk-pertiwikahuman22 <127476124+tk-pertiwikahuman22@users.noreply.github.com>
2024-10-02 05:11:56 +0000

    pertemuan1.nasikaisna.21220037

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title( "Hello World")
+name = st.text_input( "Name:")
+
+st.write('Hello', name)
